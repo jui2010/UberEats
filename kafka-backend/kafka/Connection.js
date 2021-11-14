614fd56fa6103067c79350e411f8142e7bc4afe0
Jui Thombre <jui20oct@gmail.com>
2021-11-13 20:42:21 -0800

    AWS hosting IP change

--- kafka-backend/kafka/Connection.js
+++ kafka-backend/kafka/Connection.js
@@ -1,5 +1,5 @@
 var kafka = require("kafka-node");
-const ipAddress = "18.117.128.170";
+const ipAddress = "3.142.92.54";
 function ConnectionProvider() {
   this.getConsumer = function (topic_name) {
     this.client = new kafka.KafkaClient(`${ipAddress}:2181`);
